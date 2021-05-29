b6e2f576ced4cb9eee08174718f470de9b022395
Federico D'Eredità <federico.deredita@pm.me>
2021-05-29 13:48:32 +0200

    card luoghi

--- src/pages/Luoghi.vue
+++ src/pages/Luoghi.vue
@@ -1,15 +1,17 @@
 <template>
   <q-page class="row justify-around">
-    <q-card class="col-md-6 col-xs-12 q-pt-lg" flat>
-      <q-card-section>
-        <p class="">
-          Confidando che riuscirete ad avere la meglio sul buffet dei dolci, la baldoria e i parenti molesti, vi suggeriamo alcune mete nei dintorni che valgono davvero la pena di essere visitate.
-        </p>
-      </q-card-section>
+    <div class="col-md-6 col-xs-12 q-mb-lg q-mt-lg">
 
+      <p class="">
+        Confidando che riuscirete ad avere la meglio sul buffet dei dolci, la baldoria e i parenti molesti, vi suggeriamo alcune mete nei dintorni che valgono davvero la pena di essere visitate.
+      </p>
+    </div>
+    <div class="flex-break"/>
+    <q-card class="col-md-6 col-xs-12 q-mb-lg">
       <q-card-section>
         <p>
-          Recanati (MC), conosciuta universalmente perché nel 1798 diede i natali a Giacomo Leopardi. In particolare, vi consigliamo di visitare il palazzo dove il poeta nacque e trascorse anni ‘di studi matti e disperatissimi’ prima di trasferirsi a Napoli.
+          Recanati (MC), conosciuta universalmente perché nel 1798 diede i natali a Giacomo Leopardi. In particolare, vi consigliamo di visitare il palazzo dove il poeta nacque e trascorse anni ‘di studi matti e disperatissimi’ prima di
+          trasferirsi a Napoli.
         </p>
 
         <q-img src="foto/luoghi/recanati.jpg" :ratio="4/3">
@@ -21,10 +23,13 @@
         </q-img>
 
       </q-card-section>
-
+    </q-card>
+    <div class="flex-break"/>
+    <q-card class="col-md-6 col-xs-12 q-mb-lg">
       <q-card-section>
         <p>
-          Portonovo (AN), a chi volesse fare un po’ di mare, suggeriamo di visitare la bellissima spiaggia di Mezzavalle, probabilmente la più famosa di tutto il Conero. Vi consigliamo, tuttavia, di portarvi qualcosa al sacco perché qui non ci sono lidi ma soltanto un piccolo chiosco.
+          Portonovo (AN), a chi volesse fare un po’ di mare, suggeriamo di visitare la bellissima spiaggia di Mezzavalle, probabilmente la più famosa di tutto il Conero. Vi consigliamo, tuttavia, di portarvi qualcosa al sacco perché qui
+          non ci sono lidi ma soltanto un piccolo chiosco.
         </p>
         <q-img src="https://media-cdn.tripadvisor.com/media/photo-s/06/98/f5/7a/spiaggia-di-mezzavalle.jpg" :ratio="4/3">
           <div class="absolute-bottom text-subtitle2 text-center">
@@ -33,10 +38,13 @@
         </q-img>
       </q-card-section>
 
-
+    </q-card>
+    <div class="flex-break"/>
+    <q-card class="col-md-6 col-xs-12 q-mb-lg">
       <q-card-section>
         <p>
-          Jesi (AN), città natale della sposa che negli ultimi anni ha ulteriormente arricchito la propria offerta museale. In particolare, per gli amanti dell’arte, suggeriamo di visitare la Pinacoteca Civica con i capolavori del pittore veneto, Lorenzo Lotto.
+          Jesi (AN), città natale della sposa che negli ultimi anni ha ulteriormente arricchito la propria offerta museale. In particolare, per gli amanti dell’arte, suggeriamo di visitare la Pinacoteca Civica con i capolavori del pittore
+          veneto, Lorenzo Lotto.
         </p>
         <q-img src="foto/luoghi/jesi.jpg" :ratio="4/3">
           <div class="absolute-bottom text-subtitle2 text-center">
@@ -49,12 +57,13 @@
 
 
       </q-card-section>
-
-
-
+    </q-card>
+    <div class="flex-break"/>
+    <q-card class="col-md-6 col-xs-12 q-mb-lg">
       <q-card-section>
         <p>
-          Loreto (AN), città nata a partire dalla metà del Trecento in prossimità di uno dei santuari più venerati della cristianità, che accoglie ogni anno un milione di pellegrini. Da vedere: la spettacolare basilica che custodisce la Santa Casa e il museo del tesoro del santuario.
+          Loreto (AN), città nata a partire dalla metà del Trecento in prossimità di uno dei santuari più venerati della cristianità, che accoglie ogni anno un milione di pellegrini. Da vedere: la spettacolare basilica che custodisce la
+          Santa Casa e il museo del tesoro del santuario.
         </p>
         <q-img src="foto/luoghi/loreto.png" :ratio="4/3">
           <div class="absolute-bottom text-subtitle2 text-center">
@@ -65,15 +74,16 @@
 
 
       </q-card-section>
-
-
-
+    </q-card>
+    <div class="flex-break"/>
+    <q-card class="col-md-6 col-xs-12 q-mb-lg">
       <q-card-section>
         <p>
-          Grotte di Frasassi, Genga (AN). Scoperte per caso nel 1971, queste enormi e suggestive cavità naturali che si nascondono tra le montagne figurano nella lista delle destinazioni preferite dai viaggiatori che visitano le Marche. Bellissimo il Tempietto Valadier, una chiesetta ottagonale costruita dentro una caverna.        </p>
+          Grotte di Frasassi, Genga (AN). Scoperte per caso nel 1971, queste enormi e suggestive cavità naturali che si nascondono tra le montagne figurano nella lista delle destinazioni preferite dai viaggiatori che visitano le Marche.
+          Bellissimo il Tempietto Valadier, una chiesetta ottagonale costruita dentro una caverna. </p>
         <div class="row">
-        <q-img class="col-6" src="foto/luoghi/frasassi1.jpg" :ratio="4/3"/>
-        <q-img class="col-6" src="foto/luoghi/frasassi2.jpg" :ratio="4/3"/>
+          <q-img class="col-6" src="foto/luoghi/frasassi1.jpg" :ratio="4/3"/>
+          <q-img class="col-6" src="foto/luoghi/frasassi2.jpg" :ratio="4/3"/>
         </div>
 
       </q-card-section>
@@ -90,6 +100,15 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="sass" scoped>
+.flex-break
+  flex: 1 0 100% !important
 
+.row
+  .flex-break
+    height: 0 !important
+
+.column
+  .flex-break
+    width: 0 !important
 </style>
